4abc09f384c687f76e6414349c0d01f8cc77b4d6
Anthonyz150 <anthony.didier.pro@gmail.com>
2026-02-10 22:11:39 +0100

    Promo

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Configuration directe avec vos informations
 const supabaseUrl = 'https://jpudcvzrjqzdlhitxnbw.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwdWRjdnpyanF6ZGxoaXR4bmJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNDQ1OTksImV4cCI6MjA4NTcyMDU5OX0.bXHHsEogjmMeI-zDvxvmiH1KnF_JinJ_NR94Or9dS_E';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storageKey: 'dunkly-auth-token', // Nom de la clé dans le localStorage
+    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+  },
+});
